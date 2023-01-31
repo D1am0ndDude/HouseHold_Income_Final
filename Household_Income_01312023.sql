c73c63e83b851b299241108d55043b13ff3a5ef8
Clayton Gossett <111243284+Cgossett2022@users.noreply.github.com>
2023-01-31 13:46:27 -0600

    Updated schema for SQL tables

---
+++ Household_Income_01312023.sql
@@ -0,0 +1,66 @@
+--Create Income Table --
+CREATE TABLE Income (
+	 ID VARCHAR NOT NULL,
+     year VARCHAR NOT NULL,
+	 expenditures VARCHAR NOT NULL,
+	 protocol_id VARCHAR NOT NULL,
+	 income_amount INT NOT NULL,
+	 PRIMARY KEY (ID),
+	 UNIQUE (ID)
+);
+
+-- Create Expenditure Table --
+CREATE TABLE Expenditure (
+	 ID VARCHAR NOT NULL,
+     year VARCHAR NOT NULL,
+	 expenditures VARCHAR NOT NULL,
+	 protocol_id VARCHAR NOT NULL,
+	 expenditure_amount INT NOT NULL,
+	 PRIMARY KEY (ID),
+	 UNIQUE (ID)
+);
+
+-- Test Tables --
+SELECT * FROM expenditure;
+SELECT * FROM income;
+
+-- Merge Expenditure and Income Tables
+SELECT e.id, e."year", e.expenditures, e.expenditure_amount, i.income_amount
+INTO income_expenditure
+FROM expenditure as e
+JOIN income as i
+ON e.id = i.id
+
+-- Test Table
+SELECT * FROM income_expenditure;
+
+-- Create 2019 Data Table --
+SELECT d.id, d."year", d.expenditures, d.expenditure_amount, d.income_amount
+INTO data_2019
+FROM income_expenditure as d
+WHERE d.year = '2019'
+;
+
+-- Test Table
+SELECT * FROM data_2019;
+
+-- Create 2020 Data Table --
+SELECT d.id, d."year", d.expenditures, d.expenditure_amount, d.income_amount
+INTO data_2020
+FROM income_expenditure as d
+WHERE d.year = '2020'
+;
+
+-- Create 2021 Data Table --
+SELECT d.id, d."year", d.expenditures, d.expenditure_amount, d.income_amount
+INTO data_2021
+FROM income_expenditure as d
+WHERE d.year = '2021'
+;
+
+-- Create 2022 Data Table --
+SELECT d.id, d."year", d.expenditures, d.expenditure_amount, d.income_amount
+INTO data_2022
+FROM income_expenditure as d
+WHERE d.year = '2022'
+;
